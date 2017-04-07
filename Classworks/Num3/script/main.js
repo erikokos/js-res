8a983cf2a3aafcb4751149cf10890613ca1cf0fb
erikokos <stegvy@ya.ru>
2017-04-08 01:01:17 +0300

    backup

---
+++ Classworks/Num3/script/main.js
@@ -0,0 +1,80 @@
+(function() {
+	'use strict';
+	console.log('Functions');
+/*console.log('Functions');
+
+var printFunc = function() {
+	console.log(printFunc);
+}
+
+printFunc(); //call function
+
+var printHello = function(name) {
+	console.log('HELLO ' + name);
+}
+
+printHello("Edward");
+
+var calcFunc = function(a, b) {
+	console.log('Summ = '+ (a + b));
+}
+
+calcFunc(2, 5);
+
+var printUserInfo = function(name, surname) {
+	surname = surname || 'default surname';
+	console.log(name, surname);
+}
+
+printUserInfo('Djohn', 'Smith');
+
+
+var a = 1, b = 2;
+
+typeof(a) != 'number' || typeof(b) != 'number' ? console.log('not number') : console.log('yes it is number');*/
+
+/*var name = 'Bob';
+
+function showName() {
+	var name = 'Ivan';
+	console.log(name);
+}
+
+showName();*/
+/*
+function numSumm(n) {
+	if (n === 1) return 1;
+	return n + numSumm(n - 1);
+}
+
+console.log(numSumm(4));*/
+
+
+// ЗАМЫКАНИЯ!!! 
+
+
+/*function createCounter (){
+	var callsNumber = 0;
+	return function() {
+		return callsNumber++;
+	}
+}
+
+var count = createCounter();
+
+console.log(count());
+console.log(count());
+console.log(count());
+console.log(count());
+console.log(count());
+console.log(count());
+console.log(count());
+console.log(count());
+console.log(count());*/
+
+
+
+/*запишите функцию, которая в зависимости от переданных в нее целочисленных 
+аргументов "количество дней", будет возвращать "день", "дней", "дня"*/ 
+
+}())
